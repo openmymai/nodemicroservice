612c442eebdb987315d291c46af3c803d03cf322
openmymai <maicmi@me.com>
2023-01-14 18:08:09 +0800

    Database Management, Model (MongoDB)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -22,10 +23,21 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all('*', async (req, res) => {
-    throw new NotFoundError();
+  throw new NotFoundError();
 })
 app.use(errorHandler);
 
+mongoose.set('strictQuery', false);
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+	} catch(err) {
+		console.error(err);
+	}
+};
+
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -17,7 +18,7 @@ router.post(
 			.withMessage('Password must be between 4 and 20 charactoers')
 	], 
 	// Add types for req, res
-	(req: Request, res: Response) => {
+	async (req: Request, res: Response) => {
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
@@ -25,13 +26,18 @@ router.post(
 
 		}
 
-		// if (!email || typeof email !== 'string') {
-		//     res.status(400).send('Provide a valid email');
-		// }
-		console.log('Creating a user...');
-		throw new DatabaseConnectionError();
+		const { email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
 		
-		res.send({});
+		if (existingUser) {
+			throw new BadRequestError('Email in use');
+		}
+
+		const user = User.build({ email, password });
+		await user.save();
+
+		res.status(201).send(user);
 });
 
 export { router as signupRouter };
