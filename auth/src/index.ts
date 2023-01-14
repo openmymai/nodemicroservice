08678d7bb35a9842f13d85185595dbafc39150a8
openmymai <maicmi@me.com>
2023-01-14 10:52:28 +0800

    Additional Authentication (Rearrange directory)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 
-import { currentUserRouter } from '../routes/current-user';
-import { signinRouter } from '../routes/signin';
-import { signoutRouter } from '../routes/signout';
-import { signupRouter } from '../routes/signup';
-import { errorHandler } from '../middlewares/error-handler';
-import { NotFoundError } from '../errors/not-found-error';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
